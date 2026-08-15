aa9621ffda49b2695e0b234d4a605a48c66dfc0c
Naresh Reddy <reddynaresh4928@gmail.com>
2026-08-15 23:09:14 +0530

    Debug auth env

--- frontend/lib/authApi.js
+++ frontend/lib/authApi.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-console.log(
-  "AUTH_API_URL =",
-  process.env.NEXT_PUBLIC_AUTH_API_URL
-);
-
 const authApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_AUTH_API_URL,
 });
