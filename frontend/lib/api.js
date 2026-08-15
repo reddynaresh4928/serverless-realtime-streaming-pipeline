adeb79b973fb694e25b85aa84cc48c282e66fea0
Naresh Reddy <reddynaresh4928@gmail.com>
2026-08-15 19:41:47 +0530

    Final deployment updates

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gvpp7dsnoa.execute-api.ap-south-1.amazonaws.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default api;
